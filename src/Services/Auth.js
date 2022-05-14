c0d199fb825b20d141924f836fc7bda5eda4958b
alexsancast <alexsancast@outlook.com>
2022-05-14 13:19:54 -0400

    Prueba con ForEach

--- src/Services/Auth.js
+++ src/Services/Auth.js
@@ -1,40 +1,62 @@
-const a = [
-    {
-
+const usuario = [
+    {   
+        name:"Fernelis",
         username: "fmartinez",
         pass: "aguacate"
     },
     {
+        name:"Alexander",
         username: "acastillo",
-        pass: "aguacate"
+        pass: "aguacate2"
     }
 ];
 
+usuario.forEach(element => {
+    console.log(Object.values(element));
+    
+    
+});
 
 
 
 
-const validLogin = () => {
+const username = document.getElementById('email').value;
+const password = document.getElementById('password').value;
 
-
-    let usernameU = document.getElementById('email').value;
-    let passwordP = document.getElementById('password').value;
-
-
-    if (usernameU=="fmartinez@homail.com" && passwordP==="aguacate" || usernameU=="acastillo@homail.com" && passwordP==="aguacate" ) {
-        alert("todo bien")
-
-    }
-    if (usernameU !="fmartinez@homail.com" && passwordP==="aguacate" || usernameU !="acastillo@homail.com" && passwordP==="aguacate" ) {
-        alert("Name o Password wrong")
-    }
-
-    if (usernameU=="" && passwordP=="") {
-        alert("empty fields")
-    }
+console.log(username,password);
 
 
 
 
+const imprimirUsuario = ()=>{
+
+    alert(username,password);
 }
+
+
+
+
+// const validLogin = () => {
+
+
+//     let usernameU = document.getElementById('email').value;
+//     let passwordP = document.getElementById('password').value;
+
+
+//     if (usernameU=="fmartinez@homail.com" && passwordP==="aguacate" || usernameU=="acastillo@homail.com" && passwordP==="aguacate" ) {
+//         alert("todo bien")
+
+//     }
+//     if (usernameU !="fmartinez@homail.com" && passwordP==="aguacate" || usernameU !="acastillo@homail.com" && passwordP==="aguacate" ) {
+//         alert("Name o Password wrong")
+//     }
+
+//     if (usernameU=="" && passwordP=="") {
+//         alert("empty fields")
+//     }
+
+
+
+
+// }
 
