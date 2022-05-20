efe8861af24ba16d5b713065841e77ad7dd08299
Fer <fernelis_martinez@hotmail.com>
2022-05-20 15:30:25 -0400

    Prueba con la clase user

--- src/Model/User.js
+++ src/Model/User.js
@@ -1,16 +1,23 @@
 
-// export  const user = [
-//     {username: "fmartinez", password: "12345"},
-//     {username: "acastillo", password: "123456"}
-//
-// ]
-//
-// export  const person=(username,password)=> {
-//
-//     this.username=username;
-//     this.password=password;
-//
-// }
+const arr = [];
 
-    hgfjgt
-export const p = "hola";
+export  const user = [
+    {username: "fmartinez", password: "12345"},
+    {username: "acastillo", password: "123456"}
+
+]
+
+
+export  const person = (username,password,password2) => {
+
+    arr.push({username:username,password:password,password2:password2});
+
+}
+
+
+//ver datos de los usuarios
+export const verDato=()=>{
+
+    return console.log(arr);
+
+}
--- src/Services/prueba.js
+++ src/Services/prueba.js
@@ -1,12 +1,21 @@
-import {p} from "/src/Model/User.js"
 
+ import {person} from "/src/Model/User.js"
+ import {verDato} from "/src/Model/User.js"
 
-
-const arr = [];
 
 const email = document.getElementById('email').value;
 const password = document.getElementById('password').value;
 const password2 = document.getElementById('password2').value;
 
+person(email, password,password2);
 
- console.log(p);
+
+
+
+
+
+verDato()
+
+
+
+
