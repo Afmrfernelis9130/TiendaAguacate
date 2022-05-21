b25d41ac2e339b5306df8ab611df5126bb61ce25
Fernelis <afmrfernelis@gmail.com>
2022-05-20 21:01:03 -0400

    Seguimos trancado con el codigo del registro

--- src/Services/prueba.js
+++ src/Services/prueba.js
@@ -1,20 +1,34 @@
+// import {person} from "/src/Model/User.js"
+// import {verDato} from "/src/Model/User.js"
 
- import {person} from "/src/Model/User.js"
- import {verDato} from "/src/Model/User.js"
+const arr = [];
 
+// const email = document.getElementById('email').value;
+// const password = document.getElementById('password').value;
+// const password2 = document.getElementById('password2').value;
 
-const email = document.getElementById('email').value;
-const password = document.getElementById('password').value;
-const password2 = document.getElementById('password2').value;
-
-person(email, password,password2);
-
-
-
-
+let email;
+let passwordt;
+let password2t;
 
 
-verDato()
+const person = (username, password, password2) => {
+    email = document.getElementById('email').value;
+    passwordt = document.getElementById('password').value;
+    password2t = document.getElementById('password2').value;
+
+    username = email;
+    password = passwordt;
+    password2 = password2t;
+
+
+    arr.push({u: username, p: password, p2: password2});
+
+    console.log(arr);
+}
+
+
+
 
 
 
