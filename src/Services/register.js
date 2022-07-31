92cdfb9cb76ac24e8498f82a1ad3766d0709343a
Fernelis Martinez <afmrfernelis@gmail.com>
2022-07-31 00:37:47 -0400

    MEJORA DEL LOGIN Y EL REGISTRO

--- src/Services/register.js
+++ src/Services/register.js
@@ -1,97 +1,135 @@
+const api = "/src/Model/registerUser.json"
+// const api = "/src/Model/user.json";
 
-    // import { validInput } from "./inputValid.js";
-    //Capturar los elementos del html
-    
-    // const form = document.querySelector('#form');
-    const username = document.getElementById('username');
-    const email = document.getElementById('email');
-    const password = document.getElementById('passsword');
-    const btn = document.getElementById('btn');
-
-    const passConfirm = document.querySelector('#password2');
-
-    
-    btn.addEventListener('click',(e)=>{
-        e.preventDefault();
-        validForm();
-    });
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+const btn = document.getElementById('btn');
+let isNotSave = false;
 
 
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    fetchData()
+    validForm()
 
-const validForm = ()=>{
-        
-    
+});
+
+
+const fetchData = async () => {
+    try {
+        const response = await fetch(api);
+        const data = await response.json();
+
+        if (!isNotSave) {
+            addRegister(data);
+        }
+
+    } catch (err) {
+        console.log(err);
+    }
+
+}
+
+
+function validForm() {
+
     //Capturar valor de los input y limpiar los espacios
     const usernameValue = username.value.trim();
+
     const emailValue = email.value.trim();
-    const passwordValue =password.value.trim();
-    const passConfirmValue = passConfirm.value.trim();
-   
-    
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+
     //Validar username
-     if (usernameValue ==="")  {
-            setErrorFor(username,"El campo esta vacio");
-       }
-         else if (usernameValue.length  < 6 ) {
 
-           setErrorFor(username,"Debe contener minimo 6 caracteres")
-                    }
+    if (usernameValue.length < 8) {
+        setErrorFor(username, 'Username must be at least 8 characters');
 
-        else  {
-            setSuccessFoRegister(username);
-                }
-           //Validar correo 
-          if (emailValue === "") {
-             setErrorFor (email , "El campo esta vacio")
-          } else if (!setErrorForEmail(emailValue)){
+    }
+    if (usernameValue.length > 8) {
+      setSuccessFoRegister(username);
 
-                 setErrorFor (email , "No es un correo valido");
-          }
+    }
+    if (usernameValue==="") {
+        setErrorFor(username, 'Username is required');
+        isNotSave = true;
 
-          else {
-             setSuccessFoRegister (email);
-          }
-           //Validar contrasena
+    }
+    if (usernameValue === usernameValue.toUpperCase() && !emailValue==="") {
+        setErrorFor(username, 'Username must be in UpperCase');
+        isNotSave = true;
 
-           if (passwordValue === "" ){
-              setErrorFor(password , "El campo esta vacio")
-               
-           }else if (passwordValue.length < 9){
-              setErrorFor(password , "Debe tener minimo 8 caracteres")
+    }
+    if(emailValue === "") {
+        setErrorFor(email, 'Email is required');
+        isNotSave = true;
 
-           }
+    }
+    else if(!setErrorForEmail(emailValue)) {
+        setErrorFor(email, 'Email is invalid');
+        isNotSave = true;
 
-           else
+    }
+    else {
+        setSuccessFoRegister(email);
 
-            if (passwordValue != passConfirmValue){
-              setErrorFor(passConfirm ,"Las contrasenas no coinciden")
-              setErrorFor(password ,"Las contrasenas no coinciden")
-           }
+    }
+    if(passwordValue === "") {
+        setErrorFor(password, 'Password is required');
+        isNotSave = true;
 
-           else {
-              setSuccessFoRegister(password);
-              setSuccessFoRegister(passConfirm);
-           }
+    }
+    if(password2Value === "") {
+        setErrorFor(password2, 'Password is required');
+        isNotSave = true;
 
-           
+    }
+    if(password2Value !== passwordValue) {
+        setErrorFor(password2, 'not match');
+        isNotSave = true;
+
+    }
+
+
+}
+
+function addRegister(data) {
+    const usernameValue = username.value.trim();
+    const emailValue = email.value.trim();
+    const passwordValue = password.value.trim();
+    const password2Value = password2.value.trim();
+    data.push({
+        username: usernameValue,
+        email: emailValue,
+        password: passwordValue,
+        password2: password2Value
+    });
+
+    console.log(data);
 
 }
 
 function setSuccessFoRegister(input) {
     const formControl = input.parentElement;
-     formControl.className = 'form-control success';
-                                 }
-
+    formControl.className = 'form-control success';
+}
 
 function setErrorFor(input, message) {
-  const formControl = input.parentElement;
-  const small = formControl.querySelector('small');
-  formControl.className = 'form-control error';
-  small.innerText = message;
-                           } 
-function setErrorForEmail (email){
-let regax = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
-return  regax.test(email);
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small');
+    formControl.className = 'form-control error';
+    small.innerText = message;
+}
+
+function setErrorForEmail(email) {
+    let regax = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/
+    return regax.test(email);
+
+
+
+
 }
 
 
