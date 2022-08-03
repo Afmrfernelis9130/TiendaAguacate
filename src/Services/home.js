de94e50859d7cc6ac7eaac27de56cc5dad0f0567
Fer <fernelis_martinez@hotmail.com>
2022-08-03 11:33:53 -0400

    BK

--- src/Services/home.js
+++ src/Services/home.js
@@ -1,8 +1,12 @@
 const container = document.querySelector(".container");
 const API = 'https://platzi-avo.vercel.app';
 
+const texCant = document.getElementById("icon");
+let cantidad = 0;
+const cart = []
 
-const obj = {}
+
+
 
 fetch(`${API}/api/avo`).then(prueba => prueba.json())
     .then((data) => {
@@ -18,109 +22,136 @@ fetch(`${API}/api/avo`).then(prueba => prueba.json())
 
             };
 
-            data.data.forEach(element => {
-                    //Creamos las cartas que estaran dentro del container
-                    const cuerpo = document.createElement("div");
-                    cuerpo.classList.add("card");
-                    cuerpo.dataset.id = element.id;
 
-
-                    //Adanimos la imagen de aguacate en la carta
-                    const img = document.createElement("img");
-                    img.classList.add("img-aguacate");
-                    img.src = `${API}${element.image}`;
-                    img.dataset.id = element.id;
-
-                    //Adanimos el texto del aguacate
-                    const name = document.createElement("h2");
-                    name.classList.add("name-aguacate");
-                    name.textContent = `${element.name}`;
-                    name.dataset.id = element.id;
-
-
-                    //Anadimos la descripcion del aguacate
-                    const description = document.createElement("small")
-                    description.classList.add("des-aguacate");
-                    description.textContent = (`${element.attributes.description}`);
-
-
-                    //Anadimos el precio del aguacate
-                    const price = document.createElement("p");
-                    price.classList.add("price-aguacate");
-                    price.textContent = formatPrice(element.price);
-
-                    //Anadimos el boton del aguacate
-                    const button = document.createElement("button");
-                    button.classList.add("btn-aguacate");
-                    button.textContent = "Add";
-                    button.dataset.id = element.id;
-
-
-                    //Lo inyectamos en el doc html
-                    container.appendChild(cuerpo);
-                    cuerpo.appendChild(img);
-                    cuerpo.appendChild(name);
-                    cuerpo.appendChild(description);
-                    cuerpo.appendChild(price);
-                    cuerpo.appendChild(button);
-
-                    //Eventos con los botones
-                    cuerpo.addEventListener('click', viewProduct);
-                    name.addEventListener('click', viewProduct);
-                    button.addEventListener('click', addToCart);
-
-
-                }
-            )
-
-        const cart = []
-            //Funcion para el adadir el carrito
-            function addToCart(e) {
-
-                const id = e.target.dataset.id;
-
-                const product = data.data.find(product => product.id === id);
-
-                if(!cart.includes(product)){
-                   cart.push(product);
-                    const texCant = document.getElementById("icon");
-                   texCant.innerText = cart.length;
-                   console.log(texCant);
-                   console.table(cart);
-                }else{
-                    alert("El producto ya esta en el carrito");
-                }
-
-
-
-
-
-
-
-
-            }
-
-
-            //Funcion para ver el producto
-            function viewProduct(e) {
-
-                if (e.target.classList.contains('card') || e.target.classList.contains('name-aguacate')) {
-                    const id = e.target.dataset.id;
-                    fetch(`${API}/api/avo/${id}`).then(prueba => prueba.json())
-                        .then(data => {
-                            localStorage.setItem('item', JSON.stringify(data));
-                            window.location.href = '/public/description.html';
-
-
-                        }).catch(err => {
-                        console.log(err)
-                    });
-
-
-                }
-
-            }
+             PaintCart(data, formatPrice);
 
 
         }
     )
+
+
+
+//Funcion para pintar el carrito
+function PaintCart(data, callback) {
+
+    data.data.forEach(element => {
+        //Creamos las cartas que estaran dentro del container
+        const cuerpo = document.createElement("div");
+        cuerpo.classList.add("card");
+        cuerpo.dataset.id = element.id;
+
+
+        //Adanimos la imagen de aguacate en la carta
+        const img = document.createElement("img");
+        img.classList.add("img-aguacate");
+        img.src = `${API}${element.image}`;
+        img.dataset.id = element.id;
+
+        //Adanimos el texto del aguacate
+        const name = document.createElement("h2");
+        name.classList.add("name-aguacate");
+        name.textContent = `${element.name}`;
+        name.dataset.id = element.id;
+
+
+        //Anadimos la descripcion del aguacate
+        const description = document.createElement("small")
+        description.classList.add("des-aguacate");
+        description.textContent = (`${element.attributes.description}`);
+
+
+        //Anadimos el precio del aguacate
+        const price = document.createElement("p");
+        price.classList.add("price-aguacate");
+        price.textContent = callback(element.price);
+
+        //Anadimos el boton del aguacate
+        const button = document.createElement("button");
+        button.classList.add("btn-aguacate");
+        button.textContent = "Add";
+        button.dataset.id = element.id;
+
+
+        //Lo inyectamos en el doc html
+        container.appendChild(cuerpo);
+        cuerpo.appendChild(img);
+        cuerpo.appendChild(name);
+        cuerpo.appendChild(description);
+        cuerpo.appendChild(price);
+        cuerpo.appendChild(button);
+
+        //Eventos con los botones
+        cuerpo.addEventListener('click', (e) => {
+            viewProduct(e);
+        });
+        name.addEventListener('click', (e) => {
+            viewProduct(e);
+        });
+        button.addEventListener('click', (e) => {
+            addToCart(e, data);
+        });
+
+
+    })
+}
+
+//Funcion para el adadir el carrito
+function addToCart(e, data) {
+
+    const id = e.target.dataset.id; //Obtenemos el id del aguacate
+    const product = data.data.find(product => product.id === id);
+
+    //Verificamos si el aguacate ya esta en el carrito
+    if (!cart.includes(product)) {
+        cart.push(
+            {
+                id: product.id,
+                name: product.name,
+                price: product.price,
+                sku: product.sku,
+                image: product.image,
+                attributes: product.attributes,
+                quantity: cantidad
+
+            }
+        );
+
+
+        texCant.innerText = cart.length;
+
+    } else {
+
+        cart.push(
+            {
+
+                quantity: cantidad++
+            }
+        );
+
+
+        alert("El producto ya esta en el carrito");
+    }
+
+}
+
+//Funcion para ver el producto
+function viewProduct(e) {
+
+    if (e.target.classList.contains('card') || e.target.classList.contains('name-aguacate')) {
+        const id = e.target.dataset.id;
+        fetch(`${API}/api/avo/${id}`).then(prueba => prueba.json())
+            .then(data => {
+                localStorage.setItem('item', JSON.stringify(data));
+                window.location.href = '/public/description.html';
+
+
+            }).catch(err => {
+            console.log(err)
+        });
+
+
+    }
+
+}
+
+
